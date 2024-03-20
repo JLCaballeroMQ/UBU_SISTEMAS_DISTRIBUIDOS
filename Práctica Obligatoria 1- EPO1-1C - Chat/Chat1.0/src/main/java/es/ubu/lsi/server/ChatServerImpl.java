be636fbe838a3acac65263ef9447a4b7e2671821
JLCaballeroMQ <jcm1017@alu.ubu.es>
2024-03-19 21:27:12 -0600

    Se agrega clase interna ServerThreadForClient

--- Práctica Obligatoria 1- EPO1-1C - Chat/Chat1.0/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
+++ Práctica Obligatoria 1- EPO1-1C - Chat/Chat1.0/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -1,12 +1,15 @@
 package es.ubu.lsi.server;
 
 import es.ubu.lsi.common.ChatMessage;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ChatServerImpl implements ChatServer {
+class ChatServerImpl implements ChatServer {
     private int port;
     private boolean running = false;
     private ServerSocket serverSocket;
@@ -59,6 +62,58 @@ public class ChatServerImpl implements ChatServer {
         ServerThreadForClient client = clients.remove(id);
         if (client != null) {
             client.disconnectClient();
+        }
+    }
+
+    public class ServerThreadForClient extends Thread {
+        private Socket socket;
+        private ChatServerImpl server;
+        private ObjectOutputStream out;
+        private ObjectInputStream in;
+        private int clientId;
+        private int clientCount = 0;
+
+        public ServerThreadForClient(Socket socket, ChatServerImpl server) {
+            this.socket = socket;
+            this.server = server;
+            this.clientId = ++clientCount;
+            try {
+                out = new ObjectOutputStream(socket.getOutputStream());
+                in = new ObjectInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                System.err.println("Error configurando streams: " + e.getMessage());
+            }
+        }
+
+        public void run() {
+            try {
+                ChatMessage message;
+                while ((message = (ChatMessage) in.readObject()) != null) {
+                    // Process message based on its type
+                    server.broadcast(message);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println("Error en ServerThreadForClient: " + e.getMessage());
+            } finally {
+                try {
+                    server.remove(clientId);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public void sendMessage(ChatMessage message) throws IOException {
+            out.writeObject(message);
+            out.flush();
+        }
+
+        public int getClientId() {
+            return clientId;
+        }
+
+        public void disconnectClient() throws IOException {
+            socket.close();
         }
     }
 }
